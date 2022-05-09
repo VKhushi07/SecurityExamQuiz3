136a42560d62de1a2945408b495a2db0868e64c7
VKhushi07 <94143199+VKhushi07@users.noreply.github.com>
2022-05-09 19:26:11 -0400

    Final upload

---
+++ quiz3.js
@@ -0,0 +1,484 @@
+var quiz = 
+{
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+   {
+    q : "81. A trial at which the defendant is not physically present is referred to as a trial",
+    o : [
+      "In absentia",
+      "Of Habeas corpus",
+      "In Common law",
+      "Not To Appear"
+    ],
+    a : 0
+  },
+   {
+    q : "82. Which best describes Chain of Custody",
+    o : [
+      "Destruction of evidence",
+      "Continuity of evidence",
+      "Storing of old evidence",
+      "A suspect taken into custody"
+    ],
+    a : 1
+  },
+   {
+    q : "83. Certain workers who protect public safety cannot refuse work If the refusal would endanger the life, health or safety of another person",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 0
+  },
+   {
+    q : "84. A Security guards duties can include the collection of unpaid accounts for clients",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 1
+  },
+  {
+    q : "84. As long as a Security guard has reasonable and probable grounds to believe a person has committed an indictable offence he may arrest that person",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 1
+  },
+   {
+    q : "85. In most office towers and apartment buildings what would cause elevators to automatically go to the lobby",
+    o : [
+      "An intrusion alarm",
+      "A fire alarm",
+      "Smoke",
+      "A terrorist attack"
+    ],
+    a : 1
+  },
+   {
+    q : "86. A provincially funded service for those who need assistance from a lawyer is called",
+    o : [
+      "Welfare",
+      "Legal aid",
+      "Social Funding",
+      "Legal fund of lawyers"
+    ],
+    a : 1
+  },
+   {
+    q : "87. What is the best definition of admissible evidence?",
+    o : [
+      "Relevant evidence to a proceeding that has not been excluded by a judge",
+      "Evidence that the defence wishes to have tossed",
+      "Best evidence rule",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "88. If a Security guard suspects someone of a crime he can detain the person for only as long as it takes to gain evidence",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 1
+  },
+   {
+    q : "89. 10 code for receive signal poorly",
+    o : [
+      "10-2",
+      "10-1",
+      "10-10",
+      "10-3"
+    ],
+    a : 1
+  },
+   {
+    q : "90. WHMIS Class D Includes",
+    o : [
+      "Compressible Gas",
+      "Flammable and Combustible Liquids",
+      "Oxidizing Material",
+      "Poisonous and Infectious Material"
+    ],
+    a : 3
+  },
+   {
+    q : "91. An Injunction is an order of the court requiring a party to perform some act or refrain from some conduct",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 0
+  },
+   {
+    q : "92. The purpose of tactical communications is",
+    o : [
+      "To defuse a potential situation from escalating",
+      "To have better radio communications",
+      "So others can't monitor your radio frequencies",
+      "None of the above"
+    ],
+    a : 0
+  },
+   {
+    q : "93. Under section 5 (1) of the Trespass To Property Act which of the following would NOT be a method of giving notice",
+    o : [
+      "Posted sign",
+      "In writing",
+      "Verbally",
+      "Markers",
+      "None of the above"
+    ],
+    a : 4
+  },
+  {
+    q : "94. Work place safety is the responsibility of",
+    o : [
+      "Employees",
+      "Employers",
+      "All of the above"
+    ],
+    a : 2
+  },
+   {
+    q : "95. Public relations an important part of a security guards job",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 0
+  },
+   {
+    q : "96. Under the Trespass To Property Act there are three offences under section 2. Which of the following is NOT an offence under section 2 of the act",
+    o : [
+      "Enters on premises when entry is prohibited",
+      "A person is asked to leave a house party by the owner of the property but refuses to do so",
+      "engages in an activity on premises when the activity is prohibited",
+      "None of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "97. Which of the following would NOT be a way to protect yourself from allegations of Excessive Use Of Force",
+    o : [
+      "Keep good notes",
+      "Maintain a high level of professionalism",
+      "When questioning persons always stand in their personal space",
+      "Use techniques that are authorized by law"
+    ],
+    a : 2
+  },
+   {
+    q : "98. A security guard serves the public interest",
+    o : [
+       "True",
+       "False"
+    ],
+    a : 1
+  },
+   {
+    q : "99. While patrolling a mall as a Security guard you come across an elderly woman who appears lost. As you approach the woman you realize that the woman is holding a white cane. What would this indicate to you",
+    o : [
+      "The woman had purchased a discount cane from a store in the mall",
+      "The woman may have limited vision and perhaps got disorientated",
+      "The woman appears to prefer white canes",
+      "None of the above"
+    ],
+    a : 1
+  },
+   {
+    q : "100. What is a common form of physical access control?",
+    o : [
+      "Lock & Key",
+      "Parameter control",
+      "Field alarm",
+      "Slide card"
+    ],
+    a : 0
+  },
+   {
+    q : "101. An honestly held belief in entitlement to property is called",
+    o : [
+      "Color Of Right",
+      "Land title",
+      "Land owner",
+      "Property rights"
+    ],
+    a : 0
+  },
+   {
+    q : "102. A witness who is giving testimony is giving what type of evidence",
+    o : [
+      "Direct",
+      "Trace",
+      "Similar fact",
+      "Circumstantial"
+    ],
+    a : 0
+  },
+   {
+    q : "103. When a security guard makes an arrest for theft what federal law gives them the authority to make this arrest",
+    o : [
+      "The Charter Of rights and Freedoms",
+      "The Liquor Control Act",
+      "The Criminal Code Of Canada",
+      "Section 9 of the Trespass To Property Act"
+    ],
+    a : 2
+  },
+  {
+    q : "104. Which of the following is NOT an offence under section 2 of the Trespass To Property Act",
+    o : [
+      "enters on premises when entry is prohibited",
+      "engages in an activity on premises when the activity is prohibited",
+      "does not leave the premises within 10 minutes after he or she is directed to do so by the occupier of the premises or a person authorized by the occupier,"
+    ],
+    a : 2
+  },
+   {
+    q : "105. Under section 9 of the Trespass To Property Act a Security guard may remove an individual from the property if the person is trespassing",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "106. Real evidence is",
+    o : [
+      "physical evidence",
+      "a baseball bat",
+      "a gun",
+      "All of the above"
+    ],
+    a : 3
+  },
+   {
+    q : "107. As a Security guard if you are rude to members of the public you are in violation of",
+    o : [
+      "PSISA Code Of Condu",
+      "Police Act",
+      "Human Rights Code",
+      "Criminal Code"
+    ],
+    a : 0
+  },
+   {
+    q : "108. As a Security guard you have been tasked to watch over an industrial accident scene awaiting the government investigator when a member of the media approaches you for a comment. What is the best way to handle this",
+    o : [
+      "Say No Comment then tell them they are trespassing",
+      "Ignore them",
+      "Provide them with the contact person in the company who is authorized to provide information",
+      "Serve them with Trespass notice and remove them off the property"
+    ],
+    a : 2
+  },
+   {
+    q : "109. When a Security guard is issued a notebook, who owns it",
+    o : [
+      "The Security guard",
+      "The Company who issued it",
+      "The Provincial Court",
+      "No one can own a Security notebook"
+    ],
+    a : 1
+  },
+   {
+    q : "110. Under section 7 of the Trespass To Property Act specifies two colors of markings for the purpose of giving notice. They are",
+    o : [
+      "Red and Orange",
+      "Blue and Red",
+      "Red and Yellow",
+      "Yellow and Pink"
+    ],
+    a : 2
+  },
+   {
+    q : "111. An annunciator panel is a group of lights used as a central indicator of status of equipment or systems",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "112. Non-compliance to a lawful request or direction using verbal defiance would be defined as",
+    o : [
+      "Passive resistance",
+      "Combative",
+      "Cooperative",
+      "Passive"
+    ],
+    a : 0
+  },
+   {
+    q : "113. Which best describes Chain of Custody",
+    o : [
+      "Destruction of evidence",
+      "Continuity of evidence",
+      "Storing of old evidence",
+      "A suspect taken into custody"
+    ],
+    a : 1
+  },
+  {
+    q : "114. PIPEDA enables electronic signatures",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "115. Under the Criminal Code of Canada the statute of limitations for a Summary Conviction from the date of the offence is",
+    o : [
+      "3 months",
+      "1 year",
+      "6 months",
+      "unlimited"
+    ],
+    a : 2
+  },
+   {
+    q : "116. As a Security guard which of the following would NOT be covered under the sites Emergency Measures Plan",
+    o : [
+      "Dealing with a bomb threat",
+      "Fire procedures",
+      "Earthquake evacuation procedures",
+      "Night time door closing procedures"
+    ],
+    a : 3
+  },
+   {
+    q : "117. Evidence given about an accused's history of similar bad acts is called",
+    o : [
+      "Best Evidence Rule",
+      "Similar fact evidence",
+      "Bad luck defence",
+      "Character evidence"
+    ],
+    a : 1
+  },
+   {
+    q : "118. Demonstrative Evidence include photos, x-rays, videotapes, movies, sound recordings, diagrams, forensic animation, maps, drawings, graphs, animation, simulations, and models",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 0
+  },
+   {
+    q : "119. A security guard can be called a Security Officer",
+    o : [
+      "True",
+      "False"
+    ],
+    a : 1
+  },
+   {
+    q : "120. Which of the following is NOT a Code Of Conduct violation under the PSISA",
+    o : [
+      "using profane, abusive or insulting language",
+      "exercising unnecessary force",
+      "behaviour that is either prohibited or not authorized bylaw",
+      "Accidently spilling mustard on your uniform at lunch"
+    ],
+    a : 3
+  }
+  ],
+
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+    
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) 
+      { 
+        quiz.draw(); 
+      }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
